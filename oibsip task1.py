3933e160e6b8fda35de8f63ee47673f8c532335a
Devi Krishna <96735209+Devikris@users.noreply.github.com>
2024-11-10 17:03:32 +0530

    Update oibsip task1.py

--- oibsip task1.py
+++ oibsip task1.py
@@ -1,10 +1,43 @@
 import random
 import string
 
-def generate_password(length=8):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+def generate_password(min_length, number=True, special=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_characters = string.punctuation
 
-    return password
-password = generate_password()
-print("Generated Password:", password)
+    characters = letters
+    if number:
+        characters += digits
+    if special:
+        characters += special_characters
+
+    pwd = ""
+    meet_crit = False
+    has_number = False
+    has_special = False
+
+    while not meet_crit or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        if new_char in special_characters:
+            has_special = True
+
+        meet_crit = True
+        if number:
+            meet_crit = has_number
+        if special:
+            meet_crit = meet_crit and has_special
+
+    return pwd
+
+
+min_length = int(input("Enter the minimum length: "))
+has_number = input("Do you want numbers (y/n): ").lower() == 'y'
+has_special = input("Do you need special characters (y/n): ").lower() == 'y'
+
+pwd = generate_password(min_length, has_number, has_special)
+print("Generated password:", pwd)
